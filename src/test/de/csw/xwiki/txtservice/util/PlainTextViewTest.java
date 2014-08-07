f46917a04adae6eb899ba78a04b8c79d13432bb1
Clemens Robbenhaar <c.robbenhaar@espresto.com>
2014-08-07 13:40:46 +0200

    moved the plain text extraction into a class of its own
    while doing this figurd out I forgot to include the deletion
    of txt-classes in the old package. oops ... fixing
    
    Conflicts:
    
    	src/java/de/csw/xwiki/plugin/OntologyNotificationRule.java
    	src/java/de/csw/xwiki/txtservice/XWikiTextServiceEnhancer.java
    	src/test/de/csw/xwiki/txtservice/util/PlainTextViewTest.java
    (actually just merged over the PlainTextView )

---
+++ src/test/de/csw/xwiki/txtservice/util/PlainTextViewTest.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * This file is part of the Coporate Semantic Web Project.
+ * 
+ * This work has been partially supported by the ``InnoProfile-Corporate Semantic Web" project funded by the German Federal
+ * Ministry of Education and Research (BMBF) and the BMBF Innovation Initiative for the New German Laender - Entrepreneurial Regions.
+ * 
+ * http://www.corporate-semantic-web.de/
+ * 
+ * Freie Universitaet Berlin
+ * Copyright (c) 2007-2013
+ * 
+ * Institut fuer Informatik
+ * Working Group Coporate Semantic Web
+ * Koenigin-Luise-Strasse 24-26
+ * 14195 Berlin
+ * 
+ * http://www.mi.fu-berlin.de/en/inf/groups/ag-csw/
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA or see <http://www.gnu.org/licenses/>
+ ******************************************************************************/
+package de.csw.xwiki.txtservice.util;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class PlainTextViewTest {
+
+    // private static final Logger log = Logger.getLogger(PlainTextViewTest.class);
+
+    @Test
+    public void extractMorePlainText() {
+        assertPlainText("", "");
+
+        assertPlainText("Text  and ", "Text [[link>>target]] and [[rest]]");
+        assertPlainText("Some  and macro  text.", "Some [[Text>>doc:link]] and macro {{code}}code{{/code}} text.");
+        assertPlainText("A  followed by ", "A {{html}}macro{{/html}} followed by [[link]]");
+        assertPlainText("B  followed by  and  text.", "B {{html}}macro{{/html}} followed by [[link]] and {{html}}more{{/html}} text.");
+        assertPlainText("C  followed by  and  text, finishing with a ",
+                "C {{html}}macro{{/html}} followed by [[link]] and {{html}}more{{/html}} text, finishing with a [[link>>doc:test]]");
+
+        assertPlainText("Two ", "Two [[consecutive>>target]][[links]]");
+        assertPlainText("Two  with more text", "Two [[consecutive>>target]][[links]] with more text");
+
+        assertPlainText("", "{{include document=\"Panels.PanelSheet\"/}}");
+
+    }
+
+    private void assertPlainText(String expectedPlainText, String text) {
+        PlainTextView plainTextView =  new PlainTextView(text);       
+        String plainText = plainTextView.getPlainText();
+
+        Assert.assertEquals(expectedPlainText, plainText);
+
+        for (int i = 0; i < plainText.length(); i++) {
+            int offset = plainTextView.getOriginalPosition(i);// xwt.getRealIndex(offsets, i); 
+            Assert.assertEquals(
+                    "at position " + i + " with prefix [[" + plainText.substring(0, i) + "]] : with offset " + offset,
+                    String.valueOf(plainText.charAt(i)), String.valueOf(text.charAt(offset)));
+        }
+
+    }
+
+}
